244f5dde50dc8e46dc9afbe58be6bf105111d804
MappableWorld <tech-mobile@mappable.world>
2025-02-18 15:34:10 +0300

    Publish swift package MappableMobile version 4.11.0

---
+++ Package.swift
@@ -0,0 +1,48 @@
+// swift-tools-version: 5.6
+
+import PackageDescription
+
+let package = Package(
+    name: "MappableMobileFull",
+    defaultLocalization: "en",
+    platforms: [.iOS("12.0")],
+    products: [
+        .library(
+            name: "MappableMobileFull",
+            type: .static,
+            targets: [
+                "MappableMobileFull",
+                "MappableMobileFullResources"
+            ]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "MappableMobileFull",
+            url: "https://mappableworld.s3.eu-west-3.amazonaws.com/MappableMobile-4.11.0-full.framework.zip",
+            checksum: "4a29bc0f641aa4b962cf19119cd8bd932380eae235f129c409c0f6c6917d5931"
+        ),
+        .target(
+            name: "MappableMobileFullResources",
+            path: "Resources",
+            resources: [.process("Contents")],
+            linkerSettings: [
+                .linkedFramework("CoreFoundation"),
+                .linkedFramework("Foundation"),
+                .linkedFramework("CoreLocation"),
+                .linkedFramework("UIKit"),
+                .linkedFramework("OpenGLES"),
+                .linkedFramework("SystemConfiguration"),
+                .linkedFramework("CoreGraphics"),
+                .linkedFramework("QuartzCore"),
+                .linkedFramework("Security"),
+                .linkedFramework("CoreTelephony"),
+                .linkedFramework("CoreMotion"),
+                .linkedFramework("DeviceCheck"),
+                .linkedLibrary("resolv"),
+                .linkedLibrary("c++"),
+            ]
+
+        )
+    ]
+)
