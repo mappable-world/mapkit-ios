9793f1bc0767ff6cd7a9d576485dddd7a4b36027
MappableWorld <tech-mobile@mappable.world>
2025-05-27 15:06:58 +0300

    Publish swift package MappableMobile version 4.16.0

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MappableMobileFull",
-            url: "https://mappableworld.s3.eu-west-3.amazonaws.com/MappableMobile-4.15.0-full.framework.zip",
-            checksum: "d83e35eacb44175debbc7fca34244f507bbabb75f8281f12a0a83db17ae61077"
+            url: "https://mappableworld.s3.eu-west-3.amazonaws.com/MappableMobile-4.16.0-full.framework.zip",
+            checksum: "726bf3bfc7adc91c736f770f3ad572bb18d51488fc6b030811b2b83712254bb1"
         ),
         .target(
             name: "MappableMobileFullResources",
